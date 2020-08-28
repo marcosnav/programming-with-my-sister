7867e12e698da5d63e29ce20434459e75c189e71
Marcos Navarro <m@marcosn.com>
2020-08-27 21:06:58 -0600

    Leccion 10

--- chat/chat.js
+++ chat/chat.js
@@ -4,19 +4,22 @@ const cajaDeMensaje = document.querySelector('.escribir-mensaje');
 // Obtenemos el area de los mensajes (<div id="mensajes">)
 const areaDeMensajes = document.querySelector('.tablero-de-mensajes');
 
+// Obtenemos nuestra base de datos de los mensajes
+const db = firebase.database();
+
+// Registro de mensaje de prueba
+const mensajesDB = db.ref('mensajes-chat');
+
 /*
 PROXIMOS PASOS:
-1. Cargar mensajes existentes de una base de datos y mostrarlos
-2. Al postear un nuevo mensaje, guardarlo en la base de datos
+1. Cargar mensajes existentes de una base de datos y mostrarlos ✅
+2. Al postear un nuevo mensaje, guardarlo en la base de datos ✅
 3. Darle a cada usuario que se conecta una imagen aleatoria y preguntar nombre o alias para identificarlo
 4. Cuando un usuario se conecta por segunda vez, ya no preguntar nada y usar la info que dio anteriormente
 */
 
 // Esta funcion se encarga de agregar un nuevo mensaje
-function postearMensaje() {
-  // Obtenemos el value (valor) del <input> en una variable
-  const mensajeEscrito = cajaDeMensaje.value;
-
+function postearMensaje(mensajeDeDB, fechaDelMensaje) {
   // Estructura HTML creada con JavaScript
   /* <div class="mensaje">
     <p class="mensaje-texto">
@@ -35,7 +38,7 @@ function postearMensaje() {
   // Crear parrafo de mensaje <p class="mensaje-texto">
   const parrafoHTML = document.createElement('p');
   parrafoHTML.classList.add('mensaje-texto');
-  parrafoHTML.innerText = mensajeEscrito;
+  parrafoHTML.innerText = mensajeDeDB;
 
   // Agregar parrafo de mensaje a contenedor de mensaje
   mensajeHTML.append(parrafoHTML);
@@ -50,9 +53,8 @@ function postearMensaje() {
 
   // Crear etiqueta de hora <time>
   const fechaMensajeHTML = document.createElement('time');
-  const fechaActual = moment().format('LT');
   // Le damos el valor de la fecha actual al texto de la etiqueta
-  fechaMensajeHTML.innerText = fechaActual;
+  fechaMensajeHTML.innerText = moment(fechaDelMensaje).format('LT');
 
   // Agregarle imagen y fecha a info mensaje
   infoMensajeHTML.append(imagenUsuarioHTML);
@@ -74,6 +76,50 @@ cajaDeMensaje.addEventListener('keyup', function(evento) {
   // Ejecutar funcion solamente cuando se presiona Enter
   // keyCode 13 es tecla "Enter"
   if (evento.keyCode === 13) {
-    postearMensaje();
+    mensajesDB.set({
+      texto: cajaDeMensaje.value,
+      fecha: new Date().toISOString(),
+    });
   }
 });
+
+mensajesDB.on('value', function(objetoValor) {
+  if (objetoValor && objetoValor.val()) {
+    console.log(objetoValor.val());
+    const mensaje = objetoValor.val();
+    postearMensaje(mensaje.texto, mensaje.fecha);
+  }
+});
+
+// const db = firebase.database();
+
+// const citlaEnDB = db.ref('citla');
+
+// citlaEnDB.on('value', function(objetoValor) {
+//   console.log(objetoValor.val());
+// });
+
+// setTimeout(function() {
+
+//   citlaEnDB.set({
+//     nombre: 'Citlali',
+//     apellido: 'Navarro',
+//     email: 'citla.navarro@gmail.com',
+//     hijos: 4,
+//     gatos: 2,
+//   });
+
+// }, 5 * 1000);
+
+// var testListener = firebase.database().ref('message-test');
+// testListener.on('value', function(snapshot) {
+//   console.log(snapshot.val());
+// });
+
+// setTimeout(function() {
+//   firebase.database().ref('message-test').set({
+//     username: 'bananas',
+//     email: 'm@marcosn.com',
+//   });
+// }, 5000);
+
